294154b5fbef523f40178fddd860870e63a03736
pgtc97 <79627755+pgtc97@users.noreply.github.com>
2021-04-08 16:59:22 +0700

    update

---
+++ Shapes/src/Circle.java
@@ -0,0 +1,42 @@
+public class Circle extends Shape{
+    protected double radius;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(String color, boolean filled, double radius) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public Circle(String color, boolean filled) {
+        super(color, filled);
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return radius*radius*Math.PI;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2*radius*Math.PI;
+    }
+
+    @Override
+    public String toString() {
+        return "A circle color = " + this.color + ", " + (isFilled()?"filled":"not filled") + ", radius = " + this.radius;
+    }
+}
---
+++ Shapes/src/DemoTest.java
@@ -0,0 +1,7 @@
+public class DemoTest {
+    public static void main(String[] args) {
+        Square sq = new Square("red",true,3);
+        System.out.println(sq);
+        System.out.println(sq.getArea());
+    }
+}
